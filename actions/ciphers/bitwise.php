60bfec38e04b1f180dbd84d94070aa7f9a918b22
William Funk <contact@WilliamRADFunk.com>
2015-09-12 21:34:10 -0400

    Fleshing out the Bitwise Cipher.

--- actions/ciphers/bitwise.php
+++ actions/ciphers/bitwise.php
@@ -3,37 +3,41 @@
 	// Function called from the controller to encrypt a string.
 	function bitwiseEncrypt($plaintext)
 	{
-		
-
-		return encode($plaintext);
+		$_SESSION["key"] = generateKey(strlen($plaintext));
+		return encode($plaintext, $_SESSION["key"]);
 	}
 	// Function called from the controller to decode a string.
 	function bitwiseDecode($encryptedText)
 	{
-		
-
-		return decode($encryptedText);
+		return decode($encryptedText, $_SESSION["key"]);
 	}
 	// Randomly generates a key unique to the Bitwise Cipher.
-	function generateKey()
+	function generateKey($length)
 	{
-		
-
+		$temp = "";
+		for( $i = 0; $i < $length; $i++ )
+		{
+			$temp .= chr( rand(97, 122) );
+		}
+		echo $temp, "<br>";
+		$key = bin2hex( pack('H*', $temp) );
+		echo $key, "<br>";
 		return $key;
 	}
 	// Take a perfectly good string and encodes it.
-	function encode($plaintext)
+	function encode($plaintext, $key)
 	{
 		$encryptedText = "";
 
 		$cleanText = clean($plaintext);
-
+		echo $cleanText, "<br>";
 		
+
 
 		return $encryptedText;
 	}
 	// Take a chunk of mysterious code and decode it.
-	function decode($encryptedText)
+	function decode($encryptedText, $key)
 	{
 		$decodedText = "";
 
@@ -45,12 +49,11 @@
 	// Then it converts the Ascii value into hex values.
 	function clean($plaintext)
 	{
-		$cleanText = "";
 		for( $i = 0; $i < strlen($plaintext); $i++ )
 		{
-			
+			$AscVal = ord($plaintext{$i});
+			$cleanText .= dechex($AscVal);
 		}
-
 		return $cleanText;
 	}
 ?>
