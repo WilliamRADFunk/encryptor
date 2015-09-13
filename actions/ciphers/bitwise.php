5ec63248c2728119c4e8c7751c0a0d1872a3a78c
William Funk <contact@WilliamRADFunk.com>
2015-09-12 20:31:11 -0400

    Created the shell php file for the Bitwise Cipher.

---
+++ actions/ciphers/bitwise.php
@@ -0,0 +1,24 @@
+<?php 
+	session_start();
+	// Function called from the controller to encrypt a string.
+	function foursquareEncrypt($plaintext)
+	{
+		
+
+		return encode();
+	}
+	// Function called from the controller to decode a string.
+	function foursquareDecode($encryptedText)
+	{
+		
+
+		return decode();
+	}
+	// Randomly generates a key unique to the Bitwise Cipher.
+	function generateKey()
+	{
+		
+
+		return $key;
+	}
+?>
