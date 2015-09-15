26fe71d86f906e2e1391fe61fcc6e9f742c6dc08
William Funk <contact@WilliamRADFunk.com>
2015-09-14 21:28:02 -0400

    Switched out md5 for rsa because technically md5 is not good for encryption 'and' decryption.

--- decoder.php
+++ decoder.php
@@ -26,7 +26,7 @@
 					<!--<option class="encryp-opt" value="Geometric">Geometric Cipher</option>-->
 					<!--<option class="encryp-opt" value="Hill">Hill Cipher</option>-->
 					<option class="encryp-opt" value="Keyword">Keyword Cipher</option>
-					<!--<option class="encryp-opt" value="MD5">MD5 Cipher</option>-->
+					<!--<option class="encryp-opt" value="RSA">RSA Cipher</option>-->
 					<option class="encryp-opt" value="Playfair">Playfair Cipher</option>
 					<!--<option class="encryp-opt" value="Transposition">Transposition (Rail-Fence Depth 2) Cipher</option>-->
 					<option class="encryp-opt" value="Vigniere">Vigenere Cipher</option>
