16f285662c45e317f393598467f60827ff6d9c41
William Funk <contact@WilliamRADFunk.com>
2015-09-26 01:44:58 -0400

    Restructured encode side of functionality. Now it works properly. Keygen still needs matrix inversion check. Once complete, decode can use it for most of it's functionality.

--- actions/ciphers/hill.php
+++ actions/ciphers/hill.php
@@ -22,7 +22,6 @@
 			$keyNum = (rand(0, 25) - rand(0, 25));
 			$key .= ("," . $keyNum);
 		}
-		echo $key, "</br>";
 		return $key;
 	}
 	function invertMatrix($key)
@@ -55,44 +54,50 @@
 	function encode($plaintext, $key, $sizeOfKey)
 	{
 		$encryptedText = "";
-		echo $plaintext, "</br>";
 		$cleanText = clean($plaintext);
-		echo "cleanText: &nbsp&nbsp&nbsp", $cleanText, "</br>";
 		$paddedText = padText($cleanText, $key, $sizeOfKey);
 		$paddedTextArray = explode(",", $paddedText);
-		echo "paddedTextArray: ", implode(" ", $paddedTextArray), "</br>";
 		$keyArray = explode(",", $key);
 		array_splice($keyArray, 0, 1);
-		echo "keyArray: </br>", implode(" ", $keyArray), "</br></br>";
 
-		$keyMatrix = array();
-		for($i = 0; $i < count($keyArray); $i += $j)
-		{
-			$row = array();
-			for($j = 0; $j < $sizeOfKey; $j++)
-			{
-				array_push($row, $keyArray[$i + $j]);
-			}
-			array_push($keyMatrix, $row);
-		}
+	    // Moves through text one block at a time, converts it to the 0-25 value system,
+	    // multiples key row against block as a column, mods by 26, then adds 97 to convert
+	    // back to ascii letter.
+	    for($x = 0; $x < count($paddedTextArray); $x += $sizeOfKey)
+	    {
+	        $plainBlock = array();
 
-		echo "</br></br>";
-		for($i = 0; $i < count($keyMatrix[0]); $i++)
-		{
-			foreach($keyMatrix[$i][0] as $child)
-			{
-				echo $child . " ";
-			}
-			echo "</br>";
-		}
-		echo "</br></br>";
+	        // Collects a block and converts to the 0-25 value.
+	        for($y = 0; $y < $sizeOfKey; $y++)
+	        {
+	            array_push( $plainBlock, $paddedTextArray[$x + $y] );
+	        }
 
-		for( $k = 0; $k < count($paddedTextArray); $k += $sizeOfKey )
-		{
-			$cipherBlock = encodeBlock( $paddedTextArray, $keyMatrix, $sizeOfKey, $k );
-			$encryptedText .= $cipherBlock;
-		}
-		echo "encryptedText: </br>", $encryptedText, "</br>";
+	        // Traverses the whole key, one row after the next.
+	        for($p = 0; $p < ($sizeOfKey * $sizeOfKey); $p += $sizeOfKey)
+	        {
+	            $letterTotal = 0;
+	            // Collects the sum of products of a key row and plaintext block column.
+	            for($z = 0; $z < $sizeOfKey; $z++)
+	            {
+	                $letterTotal += $keyArray[$p + $z] * $plainBlock[$z];
+	            }
+	            // Converts 0-25 value of encrypted letter back into ascii equivalent.
+	            if($letterTotal >= 0)
+	            {
+	                $encryptedText .= NumToLetter($letterTotal % 26);
+	            }
+	            else
+	            {
+	                $result = $letterTotal;
+	                while($result < 0)
+	                {
+	                    $result += 26;
+	                }
+	                $encryptedText .= NumToLetter($result);
+	            }
+	        }
+	    }
 		return $encryptedText;
 	}
 	// Take a chunk of mysterious code and decode it.
@@ -135,67 +140,20 @@
 	function padText($cleanText, $key, $sizeOfKey)
 	{
 		$cleanTextLength = count(explode(",", $cleanText));
-		echo "cleanText length: ", $cleanTextLength, "</br>";
 		if( ($cleanTextLength % $sizeOfKey) == 0 )
 		{
-			echo "padLength: 0</br>";
 			return $cleanText;
 		}
 		else
 		{
 			$paddedText = $cleanText;
 			$padLength = $sizeOfKey - ($cleanTextLength % $sizeOfKey);
-			echo "padLength: ", $padLength, "</br>";
-			for( $i = $cleanTextLength; $i <= $cleanTextLength + $padLength; $i++)
+			for( $i = $cleanTextLength; $i <= $cleanTextLength + $padLength - 1; $i++)
 			{
 				$paddedText .= "," . (rand(0, 25));
 			}
-			echo "paddedText: ", $paddedText, "</br>";
 			return $paddedText;
 		}
-	}
-	// Constructs the hill table to make decoding possible.
-	function encodeBlock($paddedTextArray, $keyMatrix, $sizeOfKey, $startIndex)
-	{
-		$toBeEncodedBlock = array();
-		$blockOfEncodedText = "";
-		for($i = $startIndex; ($i < $startIndex + $sizeOfKey) && ($i <= count($paddedTextArray)); $i++)
-		{
-			echo $i, " = ", $paddedTextArray{$i}, "</br>";
-			array_push($toBeEncodedBlock, $paddedTextArray[$i]);
-		}
-		echo "------------------</br>";
-		for($j = 0; $j < $sizeOfKey; $j++)
-		{
-			$total = 0;
-			for($k = 0; $k < $sizeOfKey; $k++)
-			{
-				echo "total = ", $total, " + ";
-				$total += $toBeEncodedBlock[$k] * $keyMatrix[$j][$k];
-				echo "(", $toBeEncodedBlock[$k], "*", $keyMatrix[$j][$k], ") = ", $total, "</br>";
-			}
-			if($total >= 0)
-			{
-				$temp = $total % 26;
-				$blockOfEncodedText .= NumToLetter($temp);
-				echo "+temp: ", $temp, " ***blockOfEncodedText = ", $blockOfEncodedText, "</br>";
-			}
-			else
-			{
-				$temp2 = $total;
-				while($temp2 < 0)
-				{
-					$temp2 += 26;
-				}
-				$blockOfEncodedText .= NumToLetter($temp2);
-				echo "-temp2: ", $temp2, " ***blockOfEncodedText = ", $blockOfEncodedText, "</br>";
-
-			}			
-		}
-		echo "------------------</br>";
-		echo "------------------</br>";
-
-		return $blockOfEncodedText;
 	}
 	// Converts a single character into the 0 to 25 integer value.
 	function letterToNum($char)
